884b41114aa5a034207011a131b3fee2a6e3250e
tomm-rodion <autovolna@gmail.com>
2023-05-07 23:22:45 +0300

    markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-import './css/styles.css';
+const form = document.querySelector('.js-search');
+const list = document.querySelector('.cuisine-list');
 
-const DEBOUNCE_DELAY = 300;
+form.addEventListener('submit', onSearch);
+function onSearch(evt) {
+  evt.preventDefault();
+  const {
+    query: { value: valueQuery },
+    cuisine: { value: valueCuisine },
+  } = evt.currentTarget.elements;
+  if (!valueQuery) {
+    alert('Поле назви страви пусте!');
+    return;
+  }
+  dishFetch(valueQuery, valueCuisine).then(data => {
+    markupCuisine(data);
+    console.dir(data.results);
+  });
+}
+
+const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
+const API_KEY = 'c7bb7efa7e314f5491bd0e25f59e7742';
+
+function markupCuisine(data) {
+  console.log(typeof data.results);
+  const markup = data.results
+    .map(item => {
+      return `<li class="resipe">
+          <img src="${item.image}" alt="${item.title}" />
+          <h2 class="title-risipe">${item.title}</h2>
+        </li>`;
+    })
+    .join('');
+  console.log(markup);
+  list.insertAdjacentHTML('beforeend', markup);
+}
+
+function dishFetch(name, valueCuisine) {
+  return fetch(
+    `${BASE_URL}?apiKey=${API_KEY}&query=${name}&cuisine=${valueCuisine}`
+  )
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+    .catch(err => console.error(err));
+}
